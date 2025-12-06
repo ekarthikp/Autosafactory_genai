0bbad0c091680cdef2efb5ba32550dd73d85f972
Karthik Krishnan <karthikkrishnan96@gmail.com>
2025-12-06 10:45:33 +0000

    chore: remove various development and test scripts and logs, and enhance gitignore rules.

--- debug_kb.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(os.getcwd())
-from src.knowledge_manager import KnowledgeManager
-
-km = KnowledgeManager()
-info = km.get_class_info('SignalServiceTranslationProps')
-print("Created By:", info.get('created_by'))
-print("References:", info.get('references'))
